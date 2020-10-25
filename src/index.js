f47d8a969aa25daf3cafaab0c9a7c9fdb5174303
Masha-Mascot-00 <mashamascot969@gmail.com>
2020-10-25 19:15:01 +0300

    feat: implement reverse integer function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n *= -1;
+    }
+    n = String(n);
+    let numberArray = n.split("");
+    n = numberArray.reverse().join("");
+    return Number(n);
+};
